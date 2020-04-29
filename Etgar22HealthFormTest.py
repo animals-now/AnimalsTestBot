c8e08100eb847b382ed5ad0ca2aa2a70588d0c24
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-04-29 20:17:16 +0300

    Update Etgar22HealthFormTest.py

--- Etgar22HealthFormTest.py
+++ Etgar22HealthFormTest.py
@@ -14,3 +14,8 @@ session.driver.quit()
 
 customFunc.sleep(300)
 session.check_in_sheets(sheet)
+
+client = customFunc.auth.get_service_sheet()
+report_sheet = client.open("Report").sheet1
+row = ["Row below assign to Etgar22 Health issues"]
+report_sheet.insert_row(row, 2)
