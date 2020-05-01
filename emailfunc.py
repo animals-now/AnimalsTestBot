4bc7c56674328cc7531be412bdafd55db17fcccc
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-05-01 20:45:10 +0300

    Update emailfunc.py

--- emailfunc.py
+++ emailfunc.py
@@ -1,62 +1,67 @@
 import base64
-import email
-import pickle
-import os.path
-import gspread
-from os.path import exists
-from sys import exit
-from time import sleep
+from email.mime.text import MIMEText
 from apiclient import errors
-from oauth2client.service_account import ServiceAccountCredentials
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import sys
-sys.path.append('/home/maor_animals_now_org/pytest')
-import auth
 
 
-service = ""
-def two_emails(service, user_id, search_string): #tell how many emails we found for search in gmail
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+    try:
+        (service.users().messages().send(userId=user_id, body=message).execute())
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def send_emails(service, row):
+    message_text = "Sheet\Petition: " + row[1] + " Email: " + row[2] + " Reason: " + row[3]
+    sender = "me"
+    subject = "Sign Up Failed"
+    to_list = ["maor@animals-now.org", ]
+    user_id = "me"
+
+    for to in to_list:
+        message = create_message(sender, to, subject, message_text)
+        send_message(service, user_id, message)
+
+
+def two_emails(service, user_id, search_string):  # tell how many emails we found for search in gmail
     try:
         search_id = service.users().messages().list(userId=user_id, q=search_string).execute()
         number_results = int(search_id['resultSizeEstimate'])
 
         if number_results == 2:
-            return ("Succeed! Thanks email and Salesforce email received")
+            return "Succeed! Thanks email and Salesforce email received"
 
         else:
-            return ("Failed - Found " + str(number_results) + " emails instead of 2")
+            return "Failed - Found " + str(number_results) + " emails instead of 2"
 
     except (errors.HttpError, errors):
-        return ('Damn!..., an error has occured... %s' % errors)
-
-
-def search_messages(service, user_id, search_string): #give messages ids according to search
-    try:
-        search_id = service.users().messages().list(userId=user_id, q=search_string).execute()
-        number_results = search_id['resultSizeEstimate']
-        final_list = list()
-        if number_results > 0:
-            message_ids = search_id['messages']
-            for ids in message_ids:
-                final_list.append(ids['id'])
-            return final_list
-        else:
-            print('Zero results for that search string, returning and empty list.')
-            return ""
-
-    except (errors.HttpError, errors):
-        print('shit..., an error has occured... %s' % errors)
-
-
-def get_message(service, user_id, msg_id): #return message body according to msg id
-
-    try:
-        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-        return('Message snippet: %s' % message['snippet'])
-#       return message
-    except (errors.HttpError, errors):
-       return('An error occurred: %s' % errors)
-
-
+        return 'Damn!..., an error has occured... %s' % errors
