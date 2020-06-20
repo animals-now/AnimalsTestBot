7b5b71284367fa653dfc34f5170a45e0d3ecd8b8
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-06-20 16:48:06 +0300

    Update Etgar22FormTest.py

--- Etgar22FormTest.py
+++ Etgar22FormTest.py
@@ -2,6 +2,7 @@ import customFunc
 
 site = "https://etgar22.co.il/?utm_source=test&utm_medium=test&utm_campaign=test"
 sheet = "אתגר 22 מבוגרים - 2019 (Responses)"
+sheet1 = "אתגר 22 - טלפניות (Responses)"
 
 session = customFunc.webFunc(site)
 session.url()
@@ -13,3 +14,4 @@ session.driver.quit()
 
 customFunc.sleep(480)
 session.check_in_sheets(sheet)
+session.check_in_sheets(sheet1)
