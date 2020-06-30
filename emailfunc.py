07e4a2cb928c3e50b76c07ce9661958d175adade
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-06-30 22:02:29 +0300

    Update emailfunc.py

--- emailfunc.py
+++ emailfunc.py
@@ -45,7 +45,7 @@ def signup_failed_email(service, row):
     message_text = "Sheet\Petition: " + row[1] + " Email: " + row[2] + " Reason: " + row[3]
     sender = "me"
     subject = "Sign Up Failed"
-    to_list = ["maor@animals-now.org", "dev@animals-now.org"]
+    to_list = ["dev@animals-now.org"]
     user_id = "me"
 
     for to in to_list:
