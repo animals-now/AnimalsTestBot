9f4f29a40d9ea40df8ccd3f849d9bc96053869ee
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-07-31 11:43:02 +0300

    Update Challenge22FormTest.py

--- Challenge22FormTest.py
+++ Challenge22FormTest.py
@@ -4,12 +4,15 @@ site = "https://challenge22.com/?utm_source=test&utm_medium=test&utm_campaign=te
 sheet = "הרשמה לאתגר 22 -  ENGLISH NEW "
 
 session = customFunc.webFunc(site)
+session.start_driver()
 session.url()
 customFunc.sleep(6)
-session.insertinfo()
+session.insert_info_to_field('FirstName', session.first_name)
+session.insert_info_to_field('LastName', session.last_name)
+session.insert_info_to_field('Email', session.email)
 session.ch_confirm_sixteen()
 session.send()
 session.driver.quit()
 
-customFunc.sleep(480)
+customFunc.sleep(600)
 session.check_in_sheets(sheet)
