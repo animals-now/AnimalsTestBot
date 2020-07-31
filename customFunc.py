797f203af5bd2bf1ba87fbd49540354dcb88aa8d
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-07-31 11:27:20 +0300

    Update customFunc.py

--- customFunc.py
+++ customFunc.py
@@ -12,9 +12,21 @@ import sys
 sys.path.append('/home/maor_animals_now_org/pytest')
 import auth
 
+
 class webFunc:
 
     def __init__(self, site):
+        self.site = site
+        self.first_name = "test+bot"
+        self.last_name = webFunc.random_char(5) + str(randint(1, 999))
+        self.email = self.first_name + self.last_name + "@animals-now.org"
+        self.phone = "052" + str(randint(1000000, 9999999))
+        self.info = [self.first_name, self.last_name, self.email, self.phone]
+
+    def start_driver(self):
+        """
+        Determine and start the selenium webdriver.
+        """
         CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
         chrome_options = Options()
         chrome_options = webdriver.ChromeOptions()
@@ -28,73 +40,99 @@ class webFunc:
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--ignore-certificate-errors')
-        self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=chrome_options)
-        self.site = site
+        self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
     def url(self):
+        """
+        Navigate to the site address that accepted upon creating instance.
+        """
         self.driver.execute_script("window.location = '{}'".format(self.site))
 
     @staticmethod
-    def random_char(length):  # create random characters
+    def random_char(length):
+        """
+        Generate random alpha string, accept length of the string.
+        """
         letters = "abcdefghijklmnopqrstuvwxyz"
         chars = ''
         for x in range(length):
             chars += chars.join(letters[randint(0, 25)])
         return chars
 
-    def insertinfo(self):  # insert info and click on the confrim check box
-        etgar = "https://etgar22.co.il/?utm_source=test&utm_medium=test&utm_campaign=test"
-        ch = "https://challenge22.com/?utm_source=test&utm_medium=test&utm_campaign=test"
-        ch_es = "https://challenge22.com/es/?utm_source=test&utm_medium=test&utm_campaign=test"
-        fname = "test+bot"
-        lname = webFunc.random_char(5) + str(randint(1, 999))
-        email = fname + lname + "@animals-now.org"
-        phone = "052" + str(randint(1000000, 9999999))
-        self.info = [fname, lname, email, phone]
-        info_index = 0
-        if self.site == etgar:
-            placeholder = ["שם פרטי", "שם משפחה", "אימייל", "טלפון"]
-        elif self.site == ch:
-            placeholder = ["First Name", "Last Name", "Email"]
-        elif self.site == ch_es:
-            placeholder = ["Nombre", "Apellido", "Email"]
-        else:  # for petitions
-            placeholder = ["שם פרטי *", "שם משפחה *", "אימייל *", "מספר טלפון"]
+    def insert_info_to_field(self, field, keys):
+        """
+        The function find all element with <input> tag and search in each element for placeholder
+        attribute. than check if any of placeholder_dict[field] is substring of placeholder value,
+        if it does send keys to this element. If the function failed or succeed to send keys to this element,
+        it will print massage to the console.
+        accept:
+        field - field to send keys
+        keys - keys to send
+        The function base on placeholder, if your form doesn't have placeholder,
+        it won't work.
+        """
 
-        for i in placeholder:
-            box = self.driver.find_element_by_xpath('//input[@placeholder="{}"]'.format(i))
-            box.send_keys(self.info[info_index])
-            info_index += 1
+        placeholder_dict = {'FirstName': ['פרטי', 'First', 'first', 'Nombre', 'nombre'],
+                            'LastName': ['משפחה', 'Last', 'last', 'Apellido', 'apellido'],
+                            'Email': ['אימייל', 'מייל', 'דוא"ל', 'דואר אלקטרוני', "דוא'ל", 'Email', 'email'],
+                            'Phone': ['טלפון', 'נייד', 'Phone', 'phone', 'Mobile', 'mobile'],
+                            'Age': ['גיל', 'Age', 'age'],
+                            'Birthday': ['שנת לידה', 'Birthday', 'birthday'],
+                            'FullName': ['שם מלא', 'full name'],
+                            }
+
+        # Find all elements with input tag (in the html <input>....</input>)
+        input_elem_list = self.driver.find_elements_by_tag_name('input')
+        for elem in input_elem_list:
+            # Get the value of the placeholder inside input (<input>placeholder="some-value"</input>
+            real_placeholder = elem.get_attribute('placeholder')
+            # If any of the item inside placeholder_dict[field] are also inside the real_placeholder,
+            # it will send keys to this element(field).
+            if any([plc in real_placeholder for plc in placeholder_dict[field]]):
+                try:
+                    elem.send_keys(keys)
+                    print(f'Succeed to send keys to: {real_placeholder} (field placeholder)')
+                except:
+                    # if failed to send keys to the field, print it to the console
+                    print(f'Failed to send keys to: {real_placeholder} (field placeholder)')
 
 
-    def collectinfo(self):
-        return self.info
-
-    def send(self):  # press on continue
+    def send(self):
+        """
+        Click on "Submit/Continue" button in etgar22.co.il,in challenge22.com and in challenge22.com/es
+        """
         send_button = self.driver.find_element_by_id('tfa_148')
         send_button.click()
 
     def etgarconfirm(self):
+        """
+        Click on "I accept the Term of use" check box in etgar22.co.il
+        """
         confrim_checkbox = self.driver.find_element_by_xpath('//label[@id="tfa_168-L"]')
         confrim_checkbox.click()
 
     def ch_confirm_sixteen(self):
+        """
+        Click on "I am 16 or older and have read the Terms of Use" check box
+        in challenge22.com and in challenge22.com/es
+        """
         sixteen_checkbox = self.driver.find_element_by_xpath('//label[@id="tfa_93-L"]')
         sixteen_checkbox.click()
 
-    def teen(self):
+
+    def teen_check_box(self):
+        """
+        Click on "I am less that 18 year old" check box in etgar22.co.il
+        """
         teen_checkbox = self.driver.find_element_by_xpath('//label[@id="tfa_90-L"]')
         teen_checkbox.click()
-        sleep(6)
-        self.send()
-        age_box = self.driver.find_element_by_xpath('//input[@placeholder="מה הגיל שלך?"]')
-        age_box.send_keys(str(randint(14, 17)))
-        self.send()
-        parent_box = self.driver.find_element_by_xpath('//input[@placeholder="מספר הטלפון של אחד ההורים"]')
-        parent_box.send_keys("050" + str(randint(1000000, 9999999)))
 
 
     def healthissue(self):
+        """
+        Sign ups to challenges's websites with random health.
+        OLD FUNCTION CHECK IT BEFORE USING!!!
+        """
         etgar = "https://etgar22.co.il/?utm_source=test&utm_medium=test&utm_campaign=test"
         ch = "https://challenge22.com/?utm_source=test&utm_medium=test&utm_campaign=test"
         ch_es = "https://challenge22.com/es/?utm_source=test&utm_medium=test&utm_campaign=test"
@@ -146,26 +184,35 @@ class webFunc:
                         pass
             except:
                 pass
-                
+
     def check_in_sheets(self, sheet):
+        """
+        Some of the signed ups transfer to google sheet, this function check if the registration arrived to
+        the google sheet. If it does, the registration delete from the google sheet.
+        If the registration doesn't arrive to the google sheet, failure email will be sent to dev.
+        Also there is a method that check if there only one row deleted from the google sheet, in case more or
+        less then one row deleted the test failed and email sent to dev.
+        """
         client = auth.get_service_sheet()  # open google sheet API client
-        service = auth.get_service_gmail() # open gmail Api
+        service = auth.get_service_gmail()  # open gmail Api
         report_sheet = client.open("Report").sheet1  # open report sheet, will insert success or failure
         self.sheet = sheet
 
+        row_failed_msg = "Registration's email not found in google sheet!!!"
+        row_success_all_msg = "Sign up succeed and removed from google sheet"
+        row_remove_more_msg = "Sign up succeed but the bot removed more than one row in the google sheet!!!"
+        row_not_remove_msg = "Sign up succeed but the bot failed to remove the test email from the google sheet"
         sign_up_sheet = client.open(self.sheet).sheet1  # open sign up form sheet
         time_now = str(datetime.today())[0:16]
-        row_failed = [time_now, self.sheet, self.info[2], "Sign up failed registration not found in the sheet"]
-        row_succeed_all = [time_now, self.sheet, self.info[2], "Sign up succeed and removed from google sheet"]
-        row_remove_more = [time_now, self.sheet, self.info[2], "Sign up succeed but remove more that one row"]
-        row_not_remove = [time_now, self.sheet, self.info[2], "Sign up succeed but the bot failed to remove the test email from the sheet"]
-        if self.sheet != "אתגר 22 - טלפניות (Responses)": # the bot check this sheet after the regular sheet checked so there is no point
-            # to wait again
-            sleep(300)
+        row_failed = [time_now, self.sheet, self.email, row_failed_msg]
+        row_succeed_all = [time_now, self.sheet, self.email, row_success_all_msg]
+        row_remove_more = [time_now, self.sheet, self.email, row_remove_more_msg]
+        row_not_remove = [time_now, self.sheet, self.email, ]
+
         try:
-            sign_up_sheet.find(self.info[2])  # search if the test email found in sign up form sheet
+            sign_up_sheet.find(self.email)  # search if the test email found in sign up form sheet
             rows_before_delete = len(sign_up_sheet.col_values(1))
-            sign_up_sheet.delete_row(sign_up_sheet.find(self.info[2]).row)
+            sign_up_sheet.delete_row(sign_up_sheet.find(self.email).row)
             rows_after_delete = len(sign_up_sheet.col_values(1))
             gap = str(rows_before_delete - rows_after_delete)
             row_failed.append(gap)  # adding the number of rows that remove
@@ -173,11 +220,12 @@ class webFunc:
             row_remove_more.append(gap)
             row_not_remove.append(gap)
             try:
-                sign_up_sheet.find(self.info[2])  # try to find the test email again
+                sign_up_sheet.find(self.email)  # try to find the test email again
                 if rows_before_delete - rows_after_delete > 1:  # check if more then one row was delete
                     report_sheet.insert_row(row_remove_more, 2)  # report that more then one row was delete
                     report_sheet.insert_row(row_not_remove, 2)  # tell us the test email found after deleting
-                    emailfunc.signup_failed_email(service, row_remove_more)  # send email, open the func in order to see to who
+                    emailfunc.signup_failed_email(service,
+                                                  row_remove_more)  # send email, open the func in order to see to who
                     emailfunc.signup_failed_email(service, row_not_remove)
                 else:
                     report_sheet.insert_row(row_not_remove, 2)  # tell us the test email found after deleting
@@ -190,31 +238,62 @@ class webFunc:
                     report_sheet.insert_row(row_succeed_all, 2)  # tell us that everything work right
         except gspread.CellNotFound:
             report_sheet.insert_row(row_failed, 2)  # tell us that test email didn't found in the sheet
-            emailfunc.signup_failed_email(service, row_failed)            
+            emailfunc.signup_failed_email(service, row_failed)
 
 
-#     def petitions_age(self):
-#         age_box = self.driver.find_element_by_xpath('//select[@placeholder="שנת לידה"]')
-#         age_box.click()
-#         select_age = self.driver.find_element_by_xpath('//option[@value="{}"]'.format(randint(1930, 2004)))
-#         select_age.click()
-
-    def petitions_send(self):  # press on continue
-        send_button = self.driver.find_element_by_xpath('//button[@type="submit"]')
+    def petitions_send(self):
+        """
+        Click on "Submit/Continue" button in animals-now.org's petitions
+        """
+        send_button = self.driver.find_element_by_css_selector('div #form_petition-form button.frm_button_submit')
         send_button.click()
 
-    def check_in_gmail(self, email_list, petitions_list):  # check for if user got thank you email and if we got email
-        # from salesforce
+    def check_in_gmail(self, email_list, petitions_list):
+        """
+        When petition registration success, the user's details transfer to salesforce. if salesforce receive
+        email with this form: test+???@animals-now.org, salesforce will send the user details to
+        test@animals-now.org.
+        This function search in test@animals-now.org inbox for email from saleforce
+        with the user's details, if the email not found the test failed and email about the failure will be send.
+        Also write registration detail in google sheet named 'Report'(test@animals-now.org is the owner of this sheet)
+        accept:
+        email_list - list of email that the bot used to sign ups.
+        petitions_list - list of petitions url the bot signed up.
+        """
         service = auth.get_service_gmail()  # open gmail API client
         client = auth.get_service_sheet()  # open google sheet API client
         report_sheet = client.open("Report").sheet1  # open report sheet, will insert success or failure
 
         petitions_index = 0
         for email_address in email_list:
-            status = emailfunc.petition_emails(service, 'me', email_address)
+            num_emails_received = emailfunc.petition_emails(service, 'me', email_address)
+            if num_emails_received == 0:
+                status = "Succeed! Salesforce email received"
+            else:
+                status = "Failed - Found " + str(num_emails_received) + " emails instead of 1"
             row_status = [str(datetime.today())[0:16], petitions_list[petitions_index], email_address, status]
             report_sheet.insert_row(row_status, 2)
             petitions_index += 1
-            if status != "Succeed! Salesforce email received":
+            if num_emails_received != 1:
                 emailfunc.signup_failed_email(service, row_status)
 
+    # def petitions_age(self):
+    #     """
+    #     Choose random birthday from the scroll in animals-now.org's petitions
+    #     """
+    #     age_box = self.driver.find_element_by_xpath('//select[@placeholder="שנת לידה"]')
+    #     age_box.click()
+    #     select_age = self.driver.find_element_by_xpath('//option[@value="{}"]'.format(randint(1930, 2004)))
+    #     select_age.click()
+
+    def add_my_name_to_petition(self):
+        """
+        Some of the times in some petition "add my name to petition" button appear before we can sign up
+        to the petition, this function click on this button.
+        REMOVE THIS FUNCTION WHEN THE A/B TEST IS DONE.
+        """
+        try:
+            button = self.driver.find_element_by_css_selector('div.add-me-to-petition-button a.fl-button')
+            button.click()
+        except:
+            print('Add my name to petition button not found, this button appear sometimes because its A/B test')
