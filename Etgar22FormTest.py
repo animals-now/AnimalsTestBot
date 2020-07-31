e99bd91abde9ba8474fcd06d266084ebdddcd303
MaorUziel <63524728+MaorUziel@users.noreply.github.com>
2020-07-31 11:44:03 +0300

    Update Etgar22FormTest.py

--- Etgar22FormTest.py
+++ Etgar22FormTest.py
@@ -5,13 +5,17 @@ sheet = "אתגר 22 מבוגרים - 2019 (Responses)"
 sheet1 = "אתגר 22 - טלפניות (Responses)"
 
 session = customFunc.webFunc(site)
+session.start_driver()
 session.url()
 customFunc.sleep(6)
-session.insertinfo()
+session.insert_info_to_field('FirstName', session.first_name)
+session.insert_info_to_field('LastName', session.last_name)
+session.insert_info_to_field('Email', session.email)
+session.insert_info_to_field('Phone', session.phone)
 session.etgarconfirm()
 session.send()
 session.driver.quit()
 
-customFunc.sleep(480)
+customFunc.sleep(600)
 session.check_in_sheets(sheet)
 session.check_in_sheets(sheet1)
