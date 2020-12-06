b5e15e0bbed62f4f9bd606f5859ba35ad7075d27
oriadam <oriadam@gmail.com>
2020-12-06 20:54:35 +0200

    fff

--- customFunc.py
+++ customFunc.py
@@ -35,7 +35,7 @@ class webFunc:
         chrome_options.add_argument("--proxy-server='direct://'")
         chrome_options.add_argument("--proxy-bypass-list=*")
         chrome_options.add_argument("--start-maximized")
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
    #     chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--no-sandbox')
